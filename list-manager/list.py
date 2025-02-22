d2bfd5ea9d9dd62e6823f1c3f19c3bd9a5709ab5
faiz <khanfaizmominshama@gmail.com>
2025-02-22 13:24:25 +0530

    Added the sorting using size

--- list-manager/list.py
+++ list-manager/list.py
@@ -14,6 +14,7 @@ tabulate.PRESERVE_WHITESPACE = True
 COLOR_TABLE = 1
 COLOR_HIGHLIGHT = 2
 
+#To convert the the size from byte to KB, MB etc..
 def format_size(size_in_bytes):
 
     for unit in ['B', 'KB', 'MB', 'GB', 'TB']:
@@ -41,18 +42,28 @@ def get_folder_size(path):
     return total_size
 
 
-def gather_directory_data(directory):
-
+def gather_directory_data(directory, sort):
+    sort_key = 0 if sort is None else 1
     data = []
+
     for entry in os.listdir(directory):
         if entry.startswith('.'):
             continue
+        
         entry_path = os.path.join(directory, entry)
         entry_name = f"/{entry}" if os.path.isdir(entry_path) else entry
         entry_size = get_folder_size(entry_path) if os.path.isdir(entry_path) else os.path.getsize(entry_path)
         entry_date = time.ctime(os.path.getmtime(entry_path))
-        data.append([entry_name, format_size(entry_size), entry_date])
+
+        data.append([entry_name, entry_size, entry_date])  
+
+    data.sort(key=lambda x: x[sort_key])
+
+    for item in data:
+        item[1] = format_size(item[1])  
+
     return data
+
 
 def generate_headers(directory, terminal_width):
 
@@ -62,20 +73,22 @@ def generate_headers(directory, terminal_width):
     return [
         f"{directory:<{col_widths[0]}}",
         f"{'Size':<{col_widths[1]}}",
-        f"{'Date of Creation':<{col_widths[2]}}"
+        f"{'Last date modified':<{col_widths[2]}}"
     ]
 
 def handle_key_input(key, cursor_row, directory, data):
     max_cursor = 3 + (len(data) * 2)
 
     if key == curses.KEY_UP and cursor_row > 3:
-        return cursor_row - 2, directory  # Move up
+        return cursor_row - 2, directory,None  # Move up
     elif key == curses.KEY_DOWN and cursor_row < max_cursor - 2:
-        return cursor_row + 2, directory  # Move down
+        return cursor_row + 2, directory, None  # Move down
     elif key == ord('q'):
-        return None, None
+        return None, None, None
+    elif key== ord('s'):
+        return cursor_row, directory, "size"
     elif key == curses.KEY_LEFT:
-        return 3, os.path.dirname(directory)  # Move back
+        return 3, os.path.dirname(directory) ,None # Move back
     elif key == 10:  # Enter key
         data_index = (cursor_row - 3) // 2
         if 0 <= data_index < len(data):
@@ -87,7 +100,7 @@ def handle_key_input(key, cursor_row, directory, data):
             else:
                 os.system(f"xdg-open '{selected_path}'")
                 return cursor_row, directory  # Stay in the same directory
-    return cursor_row, directory  # Default case (no change)
+    return cursor_row, directory , None # Default case (no change)
 
 def main(stdscr):
     curses.start_color()
@@ -97,13 +110,14 @@ def main(stdscr):
     stdscr.keypad(True)
     curses.curs_set(0)
 
+    sort=None
     current_path = os.getcwd()
     cursor_row = 3
 
     while True:
         stdscr.clear()
         terminal_width = stdscr.getmaxyx()[1]
-        directory_data = gather_directory_data(current_path)
+        directory_data = gather_directory_data(current_path, sort)
         headers = generate_headers(current_path, terminal_width)
         rows = tabulate(directory_data, headers=headers, tablefmt="grid", maxcolwidths=[None, 18]).splitlines()
 
@@ -117,11 +131,11 @@ def main(stdscr):
         stdscr.refresh()
         key = stdscr.getch()
         
-        new_cursor, new_path = handle_key_input(key, cursor_row, current_path, directory_data)
+        new_cursor, new_path, sort_struck = handle_key_input(key, cursor_row, current_path, directory_data)
         if new_cursor is None:
             break 
 
-        cursor_row, current_path = new_cursor, new_path
+        cursor_row, current_path, sort = new_cursor, new_path, sort_struck
         
     curses.endwin()
 
